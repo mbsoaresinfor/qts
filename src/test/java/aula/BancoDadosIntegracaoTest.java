636db775518293302b0afd27143f190741e78808
mabsoares <mabsoares@hcpa.edu.br>
2024-08-26 14:18:57 -0300

    insercao teste integracao.

---
+++ src/test/java/aula/BancoDadosIntegracaoTest.java
@@ -0,0 +1,29 @@
+package aula;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+public class BancoDadosIntegracaoTest {
+
+	
+	private static BancoDados banco;
+    
+    @BeforeAll
+    public static void init(){
+        banco = new BancoDados();
+    }
+    
+    @Test
+    public void testInsert(){
+        Pessoa pessoa = new Pessoa();
+        pessoa.setIdade(60);
+        pessoa.setNome("joao");
+        banco.insertPessoa(pessoa);
+        int id = pessoa.getId();
+        assertNotNull(banco.buscaPessoa(id));
+    }
+	
+}
