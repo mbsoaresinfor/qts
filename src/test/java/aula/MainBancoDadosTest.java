b35da6ace48f5ae370e84a76bd6c3b724c492ccb
mbsoares <mbsoares.infor@gmail.com>
2024-10-07 22:05:26 -0300

    aula 11

--- src/test/java/aula/MainBancoDadosTest.java
+++ src/test/java/aula/MainBancoDadosTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -62,6 +63,41 @@ public class MainBancoDadosTest {
         
         // assert
         assertFalse(resultado);
+        
+    }
+    
+    @Test
+    public void testlistarPessoasSemDadosNoBanco(){
+    	// arrange    	
+    	// configura o mock
+    	when(bancoDadosMock.listPessoa())
+    	.thenReturn(new ArrayList<Pessoa>());
+    	
+        // act
+        boolean resultado = mainBancoDados.listarPessoas();
+        
+        // assert
+        assertFalse(resultado);
+        
+    }
+    
+    @Test
+    public void testlistarPessoasComDadosNoBanco(){
+    	// arrange    	
+    	// configura o mock
+    	ArrayList<Pessoa> lista =new ArrayList<Pessoa>();
+    	lista.add(new Pessoa());
+    	lista.add(new Pessoa());
+    	lista.add(new Pessoa());
+    	
+    	when(bancoDadosMock.listPessoa())
+    	.thenReturn(lista);
+    	
+        // act
+        boolean resultado = mainBancoDados.listarPessoas();
+        
+        // assert
+        assertTrue(resultado);
         
     }
 }
